f13362a61f9a00a36a23996a706d11c611c7a1f2
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-02-17 01:35:40 -0600

    Refactor parseBodyResponse function to use 'data' parameter

--- frontend/src/utils/response-parsers.ts
+++ frontend/src/utils/response-parsers.ts
@@ -12,17 +12,17 @@ export function parseHeadersResponse(headers: [string, string][]): Record<string
  * Converts a Uint8Array or an array of numbers into a string or an array of numbers,
  * based on the type of data it represents.
  * 
- * @param {Uint8Array | number[]} body The data to be converted, which can be either a Uint8Array or an array of numbers.
+ * @param {Uint8Array | number[]} data The data to be converted, which can be either a Uint8Array or an array of numbers.
  * @returns {string | number[] | null} The converted data. If the input represents a string, it returns a string. If it represents an array of numbers, it returns an array of numbers. If the input cannot be converted to either, it returns null.
  */
-export function parseBodyResponse(body: Uint8Array | number[]): any {
+export function parseBodyResponse(data: Uint8Array | number[]): any {
     // Check if the body is a Uint8Array
-    if (body instanceof Uint8Array) {
+    if (data instanceof Uint8Array) {
         // Attempt to convert the Uint8Array to a string
         const stringData = (() => {
             try {
                 const decoder = new TextDecoder('utf-8');
-                return decoder.decode(body);
+                return decoder.decode(data);
             } catch (error) {
                 return null; // Not a string
             }
@@ -37,12 +37,12 @@ export function parseBodyResponse(body: Uint8Array | number[]): any {
         }
 
         // If it's not a string, convert to an array of numbers
-        return Array.from(body);
+        return Array.from(data);
     }
 
     // Check if the body is an array of numbers
-    if (Array.isArray(body) && body.every(element => typeof element === 'number')) {
-        return body;
+    if (Array.isArray(data) && data.every(element => typeof element === 'number')) {
+        return data;
     }
 
     // If it's neither a Uint8Array nor an array of numbers, return null
