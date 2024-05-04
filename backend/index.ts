24d0fb7d6987597c8210c890e8cc324ec29dde52
Adrián Darío Hidalgo Flores <adrian@bundly.dev>
2024-05-03 23:04:47 -0600

    chore: Update dependencies and package versions

--- backend/index.ts
+++ backend/index.ts
@@ -1,34 +1,32 @@
-import { Server, ic } from 'azle';
+import { ic } from 'azle';
 import cors from "cors";
 import express from 'express';
 
-export default Server(() => {
-    const app = express();
+const app = express();
 
-    app.use(cors());
-    app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-    // app.use((req, res, next) => {
-    //     if (ic.caller().isAnonymous()) {
-    //         res.status(401);
-    //         res.send();
-    //     } else {
-    //         next();
-    //     }
-    // });
+// app.use((req, res, next) => {
+//     if (ic.caller().isAnonymous()) {
+//         res.status(401);
+//         res.send();
+//     } else {
+//         next();
+//     }
+// });
 
-    app.post('/test', (req, res) => {
-        res.json(req.body);
-    });
-
-    app.get('/whoami', (req, res) => {
-        res.statusCode = 200;
-        res.send(ic.caller());
-    });
-
-    app.get('/health', (req, res) => {
-        res.send().statusCode = 204;
-    });
-
-    return app.listen();
+app.post('/test', (req, res) => {
+    res.json(req.body);
 });
+
+app.get('/whoami', (req, res) => {
+    res.statusCode = 200;
+    res.send(ic.caller());
+});
+
+app.get('/health', (req, res) => {
+    res.send().statusCode = 204;
+});
+
+app.listen();
